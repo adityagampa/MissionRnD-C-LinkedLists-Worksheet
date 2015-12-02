bb8af3ba392e63472302f2cae0e3d372a2c2306a
adityagampa <adityagampa149@gmail.com>
2015-12-02 21:00:54 +0530

    Submit Commit

--- src/012sllSort.cpp
+++ src/012sllSort.cpp
@@ -21,6 +21,48 @@ struct node {
 	struct node *next;
 };
 
-void sll_012_sort(struct node *head){
-	
+struct node * createnode(int data)
+{
+	struct node *n = (struct node *)malloc(sizeof(struct node));
+	n->data = data;
+	n->next = NULL;
+	return n;
+}
+struct node *insertInEnd(struct node *head, int n)
+{
+	struct node *temp = createnode(n), *last = head;
+	if (head == NULL)
+	{
+		head = temp;
+	}
+	else
+	{
+		while (last->next)
+		{
+			last = last->next;
+		}
+		last->next = temp;
+		last = temp;
+	}
+	return head;
+}
+void sll_012_sort(struct node *head)
+{
+	struct node *i = head, *j = head;
+	int data;
+	while (i)
+	{
+		j = head;
+		while (j->next)
+		{
+			if (j->data > j->next->data)
+			{
+				data = j->next->data;
+				j->next->data = j->data;
+				j->data = data;
+			}
+			j = j->next;
+		}
+		i = i->next;
+	}
 }
