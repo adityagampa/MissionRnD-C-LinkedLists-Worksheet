2496a5633896d058c9e1683462d39c6bca230f86
adityagampa <adityagampa149@gmail.com>
2015-12-02 20:54:42 +0530

    Submit Commit

--- src/numberToLinkedList.cpp
+++ src/numberToLinkedList.cpp
@@ -19,6 +19,48 @@ struct node {
 	struct node *next;
 };
 
-struct node * numberToLinkedList(int N) {
-	return NULL;
+struct node * createNode(int data)
+{
+	struct node *n = (struct node *)malloc(sizeof(struct node));
+	n->num = data;
+	n->next = NULL;
+	return n;
+}
+int reverse(int n)
+{
+	int i = 0;
+	while (n)
+	{
+		i = i * 10 + n % 10;
+		n = n / 10;
+	}
+	return i;
+}
+struct node * numberToLinkedList(int n)
+{
+	struct node *temp, *head = NULL, *last = NULL;
+	if (n == 0)
+	{
+		temp = createNode(n % 10);
+		return temp;
+	}
+	if (n<0)
+		n = n*(-1);
+	n = reverse(n);
+	while (n)
+	{
+		temp = createNode(n % 10);
+		if (head)
+		{
+			last->next = temp;
+			last = temp;
+		}
+		else
+		{
+			head = temp;
+			last = head;
+		}
+		n = n / 10;
+	}
+	return head;
 }
