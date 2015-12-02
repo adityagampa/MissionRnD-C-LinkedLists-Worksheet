3ba0ff7aa6b4935f379afd64ee5e582c1c09449a
adityagampa <adityagampa149@gmail.com>
2015-12-02 21:05:31 +0530

    Submit Commit

--- src/twodigitLinkedList.cpp
+++ src/twodigitLinkedList.cpp
@@ -21,6 +21,41 @@ struct node {
 	struct node *next;
 };
 
-int convert_sll_2digit_to_int(struct node *head){
-	return 0;
+struct node * createnode(int a, int b)
+{
+	struct node *n = (struct node *)malloc(sizeof(struct node));
+	n->digit1 = a;
+	n->digit2 = b;
+	n->next = NULL;
+	return n;
 }
+struct node *insertInEnd(struct node *head, int a, int b)
+{
+	struct node *temp = createnode(a, b), *last = head;
+	if (head == NULL)
+	{
+		head = temp;
+	}
+	else
+	{
+		while (last->next)
+		{
+			last = last->next;
+		}
+		last->next = temp;
+		last = temp;
+	}
+	return head;
+}
+int convert_sll_2digit_to_int(struct node *head)
+{
+	struct node *temp = head;
+	int n = 0;
+	while (temp)
+	{
+		n = n * 100 + temp->digit1 * 10 + temp->digit2;
+		temp = temp->next;
+	}
+	return n;
+}
+
